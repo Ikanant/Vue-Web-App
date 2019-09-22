8c987d68f247c6fe081671712dec248adff12b6a
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2019-09-21 20:34:42 -0400

    use the data model with interpolation to bind data into our component

--- src/components/DisplayDataRespondToEvents.vue
+++ src/components/DisplayDataRespondToEvents.vue
@@ -1,16 +1,31 @@
 <template>
-    <div id="simpleLinkDiv">
-        <a :href="google" target="_blank">Take me to google</a>
+    <div id="MainDiv">
+        <div id="TitleDiv">
+            <h1>{{staticPageTexts.title}}</h1>
+        </div>
+        <div id="simpleLinkDiv">
+            <a :href="google" target="_blank">Take me to google</a>
+            <br/>
+            <a v-bind:href="facebook" target="_blank">Take me to facebook</a>
+        </div>
         <br/>
-        <a :href="facebook" target="_blank">Take me to facebook</a>
+        <div id="inputBoxDiv">
+            <input type="text" style="margin-right: 20px;" placeholder="Insert Msg"/>
+            <label>{{message}}</label>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
   name: 'DisplayDataRespondToEvents',
+   
   data() {
       return {
+          staticPageTexts: {
+              title: "COOL TITLE !!"
+          },
+          message: '',
           google: "https://google.com",
           facebook: "https://facebook.com"
       }
