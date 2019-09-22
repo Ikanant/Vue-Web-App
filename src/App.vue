d3099280334133ec0cf1cf28d0ce16141227dcfc
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2019-09-21 20:10:25 -0400

    Create first vue component with basic link - no binding done yet

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DisplayDataRespondToEvents/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DisplayDataRespondToEvents from './components/DisplayDataRespondToEvents'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    DisplayDataRespondToEvents
   }
 }
 </script>
---
+++ src/components/DisplayDataRespondToEvents.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="simpleLinkDiv">
+        <a href="https://www.google.com" target="_blank">Take me to google</a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayDataRespondToEvents',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
