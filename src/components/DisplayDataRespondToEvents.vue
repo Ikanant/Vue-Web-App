3461d6fa30ef19adac183bc91a5968d43863db3a
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2019-09-21 20:17:16 -0400

    Simplest data binding scenario

--- src/components/DisplayDataRespondToEvents.vue
+++ src/components/DisplayDataRespondToEvents.vue
@@ -1,14 +1,19 @@
 <template>
     <div id="simpleLinkDiv">
-        <a href="https://www.google.com" target="_blank">Take me to google</a>
+        <a :href="google" target="_blank">Take me to google</a>
+        <br/>
+        <a :href="facebook" target="_blank">Take me to facebook</a>
     </div>
 </template>
 
 <script>
 export default {
   name: 'DisplayDataRespondToEvents',
-  props: {
-    msg: String
+  data() {
+      return {
+          google: "https://google.com",
+          facebook: "https://facebook.com"
+      }
   }
 }
 </script>
